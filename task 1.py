8d396e127a7b93fc790f13245682c54ccd4decf7
barath0000 <me25mtech11002@iith.ac.in>
2026-01-29 21:43:09 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,9 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+
+addition =float(a+b)
+subtraction=float(a-b)
+multiplication=float(a*b)
+division=float(a/b)
+print("addition =",addition,'\n'"subtraction =", subtraction,'\n',"multiplication =", multiplication,'\n'"division =", division)
+
---
+++ task 2.py
@@ -0,0 +1,4 @@
+a=input("enter first name:")
+b=input("enter second name:")
+
+print("hello" ,a+' '+b ,"welcome to python program")
